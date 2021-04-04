d2c3f85fe621e78576ec569daf8044be8243d998
Luiz Ribeiro <luizribeiro@gmail.com>
2021-04-04 12:00:08 -0500

    Successfully controlled stepper

--- esp32-s2/src/main.cpp
+++ esp32-s2/src/main.cpp
@@ -1,10 +1,28 @@
 #include <Arduino.h>
+#include <Stepper.h>
 
-void setup() { pinMode(13, OUTPUT); }
+const int stepsPerRevolution = 2038;
+Stepper myStepper(stepsPerRevolution, 4, 2, 1, 3);
+
+void setup() {
+  pinMode(13, OUTPUT);
+  myStepper.setSpeed(28);
+}
+
+void shutdownStepper() {
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+}
 
 void loop() {
   digitalWrite(13, HIGH);
-  delay(1000);
+  myStepper.step(stepsPerRevolution);
+  shutdownStepper();
+  delay(5000);
   digitalWrite(13, LOW);
-  delay(1000);
+  myStepper.step(stepsPerRevolution);
+  shutdownStepper();
+  delay(5000);
 }
